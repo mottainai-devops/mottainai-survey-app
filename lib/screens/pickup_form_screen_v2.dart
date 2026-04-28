f3bb9d3dff5680e02228129cfd3a51ea06dbb887
mottainaisurvey <245152309+mottainaisurvey@users.noreply.github.com>
2026-04-28 15:54:46 -0400

    fix: add 27 CBM DINO BIN to pickup_form_screen_v2 (active screen used by app)

--- lib/screens/pickup_form_screen_v2.dart
+++ lib/screens/pickup_form_screen_v2.dart
@@ -91,6 +91,7 @@ class _PickupFormScreenV2State extends State<PickupFormScreenV2> {
   final List<String> _binTypes = [
     '10 CBM SKIP BIN',
     '6CBM SKIP BIN',
+    '27 CBM DINO BIN',
     '240 LITRE WHEELIE BIN',
     '120 LITRE WHEELIE BIN',
     'MAMMOTH (1100 LITRE)',
